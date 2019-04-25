2d35e35b602fbb3008be9c447c7fa701f70d747f
Leonardo Fuso Nuzzo <leonardofusonuzzo@gmail.com>
2019-04-24 21:14:27 -0300

    dev - Comandos para configuração da conexão com o servidor

--- src/main/java/com/leofuso/academico/cd/bancod/client/application/commands/ConnectionManagementCommands.java
+++ src/main/java/com/leofuso/academico/cd/bancod/client/application/commands/ConnectionManagementCommands.java
@@ -1,7 +1,36 @@
 package com.leofuso.academico.cd.bancod.client.application.commands;
 
+import com.leofuso.academico.cd.bancod.client.domain.OperacaoBancaria;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
 
 @ShellComponent
 public class ConnectionManagementCommands {
+
+    private final OperacaoBancaria banco;
+
+    @Autowired
+    public ConnectionManagementCommands(OperacaoBancaria banco) {
+        this.banco = banco;
+    }
+
+    @ShellMethod("Atualiza porta")
+    public String updatePorta(int numero) {
+        banco.reconfigure(null, null, numero);
+        return banco.getURI();
+    }
+
+    @ShellMethod("Atualiza endereco")
+    public String updateEndereco(String endereco) {
+        banco.reconfigure(null, endereco, null);
+        return banco.getURI();
+    }
+
+    @ShellMethod("Atualiza protocolo")
+    public String updateProtocolo(String protocolo) {
+        banco.reconfigure(protocolo, null, null);
+        return banco.getURI();
+    }
+
 }
